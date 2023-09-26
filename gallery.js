ec2265b1a4337553cd7dd3959e14bbad53d440e1
jennayeo <ysy91231@gmail.com>
2023-09-26 23:07:57 +0900

    contents added

--- gallery.js
+++ gallery.js
@@ -1,23 +1,59 @@
-const galleryTimelineElem = document.querySelector(".gallery-timeline");
+// const galleryTimelineElem = document.querySelector(".gallery-timeline");
 const gallery = document.querySelector(".gallery");
 
-gallery.animate(
-  [
-    { opacity: 0, offset: 0 },
-    { opacity: 1, offset: 0.04 },
-    { transform: "translateX(0)", offset: 0.1 },
-    { transform: "translateX(calc(-100% + 100vw)", offset: 0.9 },
-    { opacity: 1, offset: 0.96 },
-    { opacity: 0, transform: "translateX(calc(-100% + 100vw)", offset: 1 },
-  ],
-  {
-    duration: 1,
-    fill: "both",
-    timeline: new ScrollTimeline({
-      scrollOffsets: [
-        { target: galleryTimelineElem, edge: "start", threshold: 1 },
-        { target: galleryTimelineElem, edge: "end", threshold: 1 },
-      ],
-    }),
-  }
+// gallery.animate(
+//   [
+//     { opacity: 0, offset: 0 },
+//     { opacity: 1, offset: 0.04 },
+//     { transform: "translateX(0)", offset: 0.1 },
+//     { transform: "translateX(calc(-100% + 100vw)", offset: 0.9 },
+//     { opacity: 1, offset: 0.96 },
+//     { opacity: 0, transform: "translateX(calc(-100% + 100vw)", offset: 1 },
+//   ],
+//   {
+//     duration: 1,
+//     fill: "both",
+//     timeline: new ScrollTimeline({
+//       scrollOffsets: [
+//         { target: galleryTimelineElem, edge: "start", threshold: 1 },
+//         { target: galleryTimelineElem, edge: "end", threshold: 1 },
+//       ],
+//     }),
+//   }
+// );
+
+const airplaneScrollTimeline = document.querySelector(
+  ".airplane-scroll-timeline"
 );
+
+// Define the options for the Intersection Observer
+const options = {
+  root: null, // Use the viewport as the root
+  rootMargin: "0px", // No margin
+  threshold: 0.6, // Trigger when 20% of the element is visible
+};
+
+// Callback function to handle the intersection
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Check if the element is entering or leaving the viewport
+      const isEnteringViewport = entry.boundingClientRect.top >= 0;
+      console.log(isEnteringViewport);
+      // Add or remove the appropriate class based on scrolling direction
+      if (isEnteringViewport) {
+        entry.target.classList.add("gallery-active");
+      } else {
+        // entry.target.classList.remove("text-block-career-active");
+      }
+
+      // Once the animation is complete, remove the observer to save resources
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+// Create an Intersection Observer
+const observer = new IntersectionObserver(handleIntersection, options);
+observer.observe(gallery);
+// });
--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 import "./scroll-timeline.js";
 import "./airplane.js";
 import "./hello.js";
-// import "./gallery.js";
+import "./gallery.js";
 import "./track.js";
 import "./carrer.js";
 import "./education.js";
-
 import "./modal.js";
 
 document.body.animate(
   [
-    { backgroundColor: "midnightblue" },
-    { backgroundColor: "dodgerblue" },
+    // { backgroundColor: "midnightblue" },
+    // { backgroundColor: "dodgerblue" },
+    { backgroundColor: "#304752" },
+    // { backgroundColor: "#F05454" },
+
     { backgroundColor: "coral" },
   ],
   {
@@ -24,3 +26,18 @@ document.body.animate(
     }),
   }
 );
+var copyText = document.getElementById("copyItem");
+
+copyText.onclick = function () {
+  navigator.clipboard
+    .writeText("jenna.jiyoung.yeo@gmail.com")
+    .then(() => {
+      // Success callback
+      alert("이메일이 복사되었습니다!");
+    })
+    .catch((error) => {
+      // Error callback
+      console.error("Error copying to clipboard:", error);
+    });
+};
+//
